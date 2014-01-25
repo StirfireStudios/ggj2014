f0902ef30405de4c563b777ac3c296585235763e
Maya Kerr <maya@stirfire.net>
2014-01-25 14:01:26 +0800

    Basic game timer, manually advances at the moment and broadcasts a game tick

---
+++ Assets/game/GameTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour
+{
+	Rect advanceRect;
+	bool alreadyAdvanced = false;
+
+	void Awake()
+	{
+		float inset = 8;
+		float size = 64;
+		advanceRect = new Rect(Screen.width - size - inset, inset, size, size);
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button(advanceRect, ">") || Input.GetKeyDown(KeyCode.Period))
+		{
+			if (!alreadyAdvanced)
+			{
+				MessagePasser.send("game-tick", null);
+			}
+			alreadyAdvanced = true;
+		}
+		else
+		{
+			alreadyAdvanced = false;
+		}
+	}
+}
