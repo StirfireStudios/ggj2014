e01183530c7d8d05fec5e99c37a2d3cb1a690b3b
Maya Kerr <maya@stirfire.net>
2014-01-25 13:30:24 +0800

    Rough first go at character dialogue state machine

---
+++ Assets/game/passengers/DialogueMachine.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Twee;
+
+public class DialogueMachine
+{
+	TweeNode currentNode;
+	bool paused = false;
+
+	public DialogueMachine(TweeNode startNode)
+	{
+		currentNode = startNode;
+	}
+
+	public void OnTick()
+	{
+		if (paused)
+		{
+			return;
+		}
+
+		TweeLink link = currentNode.Links[1];
+		currentNode = link.Node;
+	}
+
+	public void Pause()
+	{
+		paused = true;
+	}
+
+	public void Resume()
+	{
+		paused = false;
+	}
+
+	public string Text
+	{
+		get
+		{
+			return currentNode.Text;
+		}
+	}
+
+	public TweeNode CurrentNode
+	{
+		get
+		{
+			return currentNode;
+		}
+	}
+}
