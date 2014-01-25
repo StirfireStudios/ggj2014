0ae56e0382cf1d5b33633dee29cf849a69fc2e98
Maya Kerr <maya@stirfire.net>
2014-01-25 18:51:23 +0800

    Time now advances on its own

--- Assets/game/GameTimer.cs
+++ Assets/game/GameTimer.cs
@@ -3,14 +3,28 @@ using System.Collections;
 
 public class GameTimer : MonoBehaviour
 {
+	public float tickLength = 5;
+
 	Rect advanceRect;
 	bool alreadyAdvanced = false;
+
+	float timer = 0;
 
 	void Awake()
 	{
 		float inset = 8;
 		float size = 64;
 		advanceRect = new Rect(Screen.width - size - inset, inset, size, size);
+	}
+
+	void Update()
+	{
+		timer = timer + Time.deltaTime;
+		if (timer >= tickLength)
+		{
+			MessagePasser.send("game-tick", null);
+			timer = timer - tickLength;
+		}
 	}
 
 	void OnGUI()
