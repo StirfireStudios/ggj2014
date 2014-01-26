7f8f0ead1bf5681bfc43741909c25f460f03112d
Brendan Ragan <lordmortis@gmail.com>
2014-01-26 15:36:06 +0800

    Camera Pan and Tilt

--- Assets/game/PlayerCamera.cs
+++ Assets/game/PlayerCamera.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class PlayerCamera : MonoBehaviour {
 
-	public float MaxCameraTilt = 90.0f;
-	public float JoyStickCameraPanSensitivity = 1.0f;
-	public float MouseCameraPanSensitivity = 15.0f;
-	
+	public float MaxCameraTilt = 80.0f;
+	public float JoyStickCameraPanSensitivity = 3.0f;
+	public float MouseCameraPanSensitivity = 10.0f;
+
+	public float JoyStickCameraTiltSensitivity = 3.0f;
+	public float MouseCameraTiltSensitivity = 10.0f;
+
 	void Awake() {
 		MessagePasser.subscribe("controller-status", OnControllerStatus);
 		MessagePasser.subscribe("camera", OnCameraMessage);
@@ -17,14 +20,20 @@ public class PlayerCamera : MonoBehaviour {
 	
 	}
 
-	void moveCameraBy(float amount) {
-		_cameraPanY += Input.GetAxis("RightVertical") * JoyStickCameraPanSensitivity;
-		Mathf.Clamp (_cameraPanY, -MaxCameraTilt, MaxCameraTilt);
-
-		Debug.Log ("Transform by: " + _cameraPanY);
-		transform.eulerAngles = new Vector3(_cameraPanY, 0);
+	void tiltCameraBy(float amount) {
+		_cameraTilt += amount;
+		_cameraTilt = Mathf.Clamp (_cameraTilt, -1 * MaxCameraTilt, MaxCameraTilt);
+		transform.eulerAngles = new Vector3(_cameraTilt, 0);
+		Debug.Log ("Camera Tilt: " + _cameraTilt);
 	}
-	
+
+	void panCameraBy(float amount) {
+		_cameraPan += amount;
+		_cameraPan = _cameraPan % 360.0f;
+		Debug.Log ("Camera Pan: " + _cameraPan);
+		transform.eulerAngles = new Vector3(0, _cameraPan);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (!_enabled) {
@@ -35,17 +44,16 @@ public class PlayerCamera : MonoBehaviour {
 		Vector3 vcamera;
 
 		if (_controller) {
-			moveCameraBy(Input.GetAxis("RightVertical") * JoyStickCameraPanSensitivity);
+			tiltCameraBy(Input.GetAxis("RightVertical") * JoyStickCameraTiltSensitivity);
+			panCameraBy(Input.GetAxis("RightHorizontal") * JoyStickCameraPanSensitivity);
 		} else {
 			if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
 
 			} else {
-				Debug.Log ("Mouse: " + Input.GetAxis("Mouse Y") * MouseCameraPanSensitivity);
-	           moveCameraBy(Input.GetAxis("Mouse Y") * MouseCameraPanSensitivity);
+				tiltCameraBy(-1 * Input.GetAxis("Mouse Y") * MouseCameraTiltSensitivity);
+				panCameraBy(-1 * Input.GetAxis("Mouse X") * MouseCameraPanSensitivity);
 			}
 		}
-		
-
 	}
 
 	public void OnControllerStatus(string message, string arg) {
@@ -62,7 +70,8 @@ public class PlayerCamera : MonoBehaviour {
 			_enabled = true;
 	}
 
-	private float _cameraPanY = 0.0f;
+	private float _cameraTilt = 0.0f;
+	private float _cameraPan = 0.0f;
 	private bool _controller = false;
 	private bool _enabled = true;
 }
