947205f3652196819195a9d497a0ef4f6ae05dc2
Brendan Ragan <lordmortis@gmail.com>
2014-01-26 16:35:41 +0800

    add credits and start scene

---
+++ Assets/title/credits.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class credits : MonoBehaviour {
+
+	public void OnGUI () {
+		float width = 150;
+		float height = 100;
+
+		float x = Screen.width - width;
+		float y = Screen.height - height;
+		if (GUI.Button (new Rect (x,y,width,height), "Main Menu")) {
+			Application.LoadLevel ("title");
+		}
+	}
+
+	public void Awake() {
+		TextAsset creditsFile = Resources.Load<TextAsset>("credits");
+		text = GetComponent<GUIText>();
+		text.richText = true;
+		text.text = creditsFile.text;
+		starty = -text.GetScreenRect().height / 2;
+		starty = starty - Screen.height / 2;
+		text.pixelOffset = new Vector2(0, starty);
+		maxy = text.GetScreenRect().height / 2 + Screen.height / 2;
+	}
+
+	public void Update() {
+		Vector2 offset = text.pixelOffset;
+		offset.y += 2;
+		if (text.pixelOffset.y > maxy) {
+			offset.y = starty;
+		}
+		text.pixelOffset = offset;
+	}
+
+	private float maxy, starty;
+	private GUIText text;
+}
