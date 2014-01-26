c6f8a14beab033c4e9785b6ea399ea07a317646c
Brendan Ragan <lordmortis@gmail.com>
2014-01-26 13:13:00 +0800

    Controller joystick works somewhat

--- Assets/game/Player.cs
+++ Assets/game/Player.cs
@@ -1,14 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
 	public static Player Instance;
 
 	public string characterName;
 
-	void Awake()
-	{
+	IEnumerator CheckForControllers() {
+		while(true) {
+			string[] controllers = Input.GetJoystickNames();
+			if (!_controller && controllers.Length > 0) {
+				_controller = true;
+				MessagePasser.send("controller-status", "connected");
+			} else if (_controller && controllers.Length == 0) {
+				_controller = false;
+				MessagePasser.send("controller-status", "disconnected");
+			}
+			yield return new WaitForSeconds(1f);
+		}
+	}
+	
+	void Awake() {
 		Instance = this;
 	}
+
+	void Start() {
+		StartCoroutine(CheckForControllers());
+	}
+
+	private bool _controller = false;
 }
---
+++ Assets/game/PlayerCamera.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	public float MaxCameraPanY = 90.0f;
+	public float CameraPanSensitivity = 1.0f;
+	
+	void Awake() {
+		MessagePasser.subscribe("controller-status", OnControllerStatus);
+		MessagePasser.subscribe("camera", OnCameraMessage);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!_enabled) {
+			// Reset all stuffs to zero.
+			return;
+		}
+
+		if (_controller) {
+			_cameraPanY += Input.GetAxis("RightVertical") * CameraPanSensitivity;
+
+			if (_cameraPanY < -MaxCameraPanY) {
+				_cameraPanY = -MaxCameraPanY;
+			} else if (_cameraPanY > MaxCameraPanY) {
+				_cameraPanY = MaxCameraPanY;
+			}
+
+			Vector3 vcamera = new Vector3(_cameraPanY, 0);
+
+			if (camera != null) {
+				Debug.Log ("Transform Camera!");
+				transform.eulerAngles = vcamera;
+			}
+		} else {
+			// Mouse keyboard / touchscreen
+		}
+	}
+
+	public void OnControllerStatus(string message, string arg) {
+		if (arg == "connected")
+			_controller = true;
+		else if (arg == "disconnected")
+			_controller = false;
+	}
+
+	public void OnCameraMessage(string message, string arg) {
+		if (arg == "disable")
+			_enabled = false;
+		else if (arg == "enable")
+			_enabled = true;
+	}
+
+	private float _cameraPanY = 0.0f;
+	private bool _controller = false;
+	private bool _enabled = true;
+}
